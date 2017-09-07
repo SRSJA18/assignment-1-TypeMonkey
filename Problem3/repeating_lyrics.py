e1fd04384ceefa1a4297b48e5b1d71b0cfcb7000
Stephen Cerruti <scerruti+github@e3civichigh.com>
2017-09-07 08:33:25 -0700

    Create repeating_lyrics.py
    This assignment allows the scholars to demonstrate some creativity and reinforces the use of variables in place of repeating strings.

---
+++ Problem3/repeating_lyrics.py
@@ -0,0 +1,29 @@
+# Problem 3 Extension: Repeating Lyrics
+#
+# Many songs use repitition. We can use variables to manage that repetition when printing out the lyrics.
+#
+# Here is the chorus for Rick Astley's 'Never Gonna Give You Up' 
+#
+# Never gonna give you up
+# Never gonna let you down
+# Never gonna run around and desert you
+# Never gonna make you cry
+# Never gonna say goodbye
+# Never gonna tell a lie and hurt you
+#
+# Here is a program that outputs that chorus:
+#
+root = "Never gonna"
+print root, "give you up"
+print root, "let you down"
+print root, "run around and desert you"
+print root, "make you cry"
+print root, "say goodbye"
+print root, "tell a lie and hurt you"
+print # blank line
+#
+# Option 1: Choose a song or chorus and write the program to output the lyrics using variables
+# Option 2a: If you know about Python lists you could simplify the program above using a loop.
+# Option 2b: If you completed 2a you may have noticed that all but one line has the form
+#            'Never gonna _____ you ____' with the second blank optional. Can you rewrite the program
+#            using variables? How will you handled 'say goodbye'?
